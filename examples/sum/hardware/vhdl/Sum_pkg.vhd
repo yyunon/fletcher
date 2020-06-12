5ee125a9a4220ab5955fd52c96c38a56c88fe6c7
Akos Hadnagy <akos.hadnagy@gmail.com>
2020-06-12 19:53:40 +0200

    Added new sum example files

---
+++ examples/sum/hardware/vhdl/Sum_pkg.vhd
@@ -0,0 +1,83 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: Ákos Hadnagy
+-- 
+-- Create Date: 05/29/2020 03:41:48 PM
+-- Design Name: 
+-- Module Name: SequenceStream - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_misc.all;
+
+package Sum_pkg is
+
+  component SumOp is
+  generic (
+
+    -- Width of the stream data vector.
+    DATA_WIDTH                  : natural
+   
+
+  );
+  port (
+
+    -- Rising-edge sensitive clock.
+    clk                          : in  std_logic;
+
+    -- Active-high synchronous reset.
+    reset                        : in  std_logic;
+
+    --OP1 Input stream.
+    op1_valid                    : in  std_logic;
+    op1_ready                    : out std_logic;
+    op1_data                     : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+    
+    --OP2 Input stream.
+    op2_valid                    : in  std_logic;
+    op2_ready                    : out std_logic;
+    op2_data                     : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+
+    -- Output stream.
+    out_valid                    : out std_logic;
+    out_ready                    : in  std_logic;
+    out_data                     : out std_logic_vector(DATA_WIDTH-1 downto 0)
+  );
+  end component;
+  
+  component Sum_ExampleBatch_Reduce is
+  generic (
+    INDEX_WIDTH : integer := 32;
+    TAG_WIDTH   : integer := 1
+  );
+  port (
+    clk                          : in  std_logic;
+    reset                        : in  std_logic;
+    
+    in_valid                     : in  std_logic;
+    in_ready                     : out std_logic;
+    in_last                      : in  std_logic;
+    in_data                      : in  std_logic_vector(63 downto 0);
+    
+    out_valid                    : out std_logic;
+    out_ready                    : in  std_logic;
+    out_data                     : out std_logic_vector(63 downto 0)
+    
+  );
+  end component;
+
+
+end Sum_pkg;
